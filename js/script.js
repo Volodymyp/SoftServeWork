78d7f8435af3030a27785c83775c01124ebd685a
Volodymyp <volodymyp1989@gmail.com>
2021-02-12 00:52:05 +0200

    about and neigh block css

--- js/script.js
+++ js/script.js
@@ -11,3 +11,68 @@ window.onscroll = () => {
 	}
 }
 
+
+
+//background image
+function ibg() {
+
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+
+ibg();
+
+document.querySelectorAll('.swiper-container').forEach(function (elem) {
+	new Swiper(elem, {
+		slidesPerView: 1,
+		loop: true,
+		autoplay: {
+			delay: 5000,
+		},
+		spaceBetween: 10,
+		navigation: {
+			nextEl: elem.nextElementSibling.nextElementSibling,
+			prevEl: elem.nextElementSibling,
+		},
+		breakpoints: {
+			525: {
+				slidesPerView: 2,
+				spaceBetween: 20,
+			},
+			767: {
+				slidesPerView: 3,
+				spaceBetween: 30,
+			},
+			991: {
+				slidesPerView: 4,
+				spaceBetween: 30,
+			},
+		}
+	});
+});
+
+for (let i of document.querySelectorAll(".count, .counter__content-title")) {
+
+	let numberTop = i.getBoundingClientRect().top,
+		start = +i.innerHTML,
+		end = +i.dataset.max;
+
+	window.addEventListener('scroll', function onScroll() {
+		if (window.pageYOffset > numberTop - window.innerHeight / 1.5) {
+			this.removeEventListener('scroll', onScroll);
+			let interval = this.setInterval(function () {
+				i.innerHTML = ++start;
+				if (start == end) {
+					clearInterval(interval);
+				}
+			}, 10);
+		}
+	});
+}
+
+
+
